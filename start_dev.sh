7c491769be879531b484005b6e80dd8af094606c
root <edward>
2014-07-23 14:16:37 -0700

    add check exitin running erlang node and try to shut it down prior to start server

--- start_dev.sh
+++ start_dev.sh
@@ -1,6 +1,29 @@
 #! /usr/bin/env sh
 echo on
 set -e
+export ERLANG_NODE=folsom_webmachine
 
+# make sure node not already running and node name unregistered
+check_start()
+{
+    epmd -names 2>/dev/null | grep -q " ${ERLANG_NODE%@*} " && {
+        ps ux | grep -v grep | grep -q " $ERLANG_NODE " && {
+            echo "ERROR: The  node '$ERLANG_NODE' is already running."
+            exit 4
+        } || {
+            ps ux | grep -v grep | grep -q beam && {
+                echo "ERROR: The node '$ERLANG_NODE' is registered,"
+                echo " but no related beam process has been found."
+                echo "Shutdown all other erlang nodes, and call 'epmd -kill'."
+                exit 5
+            } || {
+                epmd -kill >/dev/null
+            }
+        }
+    }
+    echo "There is no erlang node with name ${ERLANG_NODE} running"
+}
 
-erl -pa ebin deps/*/ebin -sname folsom_webmachine -s folsom_webmachine
+check_start()
+sleep 1
+erl -pa ebin deps/*/ebin -sname ${ERLANG_NODE} -s folsom_webmachine

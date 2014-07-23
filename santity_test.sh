4e1551d7cec83550caac244d6b47d3fb30561285
root <edward>
2014-07-23 13:49:42 -0700

    update comment

--- santity_test.sh
+++ santity_test.sh
@@ -5,10 +5,10 @@ echo "How much memory Erlang VM is using"
 curl http://localhost:5565/_memory
 sleep 1
 clear
-echo "Dump system stats of Erlang VM"
+echo "Dump stats of Erlang VM"
 curl http://localhost:5565/_statistics
 sleep 1
 clear
-echo "Dump the entire system statistics of the environment"
+echo "Dump the entire system informatio of Erlang VM"
 curl http://localhost:5565/_system
 sleep 1
